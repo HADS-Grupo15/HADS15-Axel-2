a61ca636a83852968fcd21cc2ff8f7067da34292
HADS-Grupo15 <36604470+HADS-Grupo15@users.noreply.github.com>
2018-03-12 15:45:41 +0100

    Initial commit

---
+++ WebApplication1/Inicio.aspx.vb
@@ -0,0 +1,31 @@
+﻿
+Imports AccesoDatosSQL.accesodatosSQL
+
+Public Class Login
+    Inherits System.Web.UI.Page
+
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+
+    End Sub
+
+    Protected Sub submit_Click(sender As Object, e As EventArgs) Handles submit.Click
+
+        'Comprobar si las validaciones (concretamente CustomValidator) son correctas
+        If (Page.IsValid) Then
+            Dim logResult As Integer
+
+            logResult = comprobarUsuario(txtMail.Text, txtPass.Text)
+
+            If logResult = -1 Then
+                lblAns.Text = "Ese email no se encuentra registrado"
+            ElseIf logResult = 0 Then
+                lblAns.Text = "Contraseña errónea"
+            Else
+                Session("userEmail") = txtMail.Text
+                'Redirigir a aplicación principal
+            End If
+        End If
+
+    End Sub
+
+End Class
